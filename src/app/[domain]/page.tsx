821e24333b1756562a3198eba46b00a11153320d
Usman Khalid <ukhalid@jobcase.com>
2023-09-26 15:34:39 +0500

    config api added

--- src/app/[domain]/page.tsx
+++ src/app/[domain]/page.tsx
@@ -1,16 +1,35 @@
 import { layouts } from '@/constant/layouts';
 
+async function getData(domain:string) {
+  const res = await fetch(`https://blazorstagingapi.azurewebsites.net/api/Config?URL=${domain}/`)
+ 
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
+}
+ 
+
 const HomePage = async ({ params }: { params: { domain: string } }) => {
+  // BE
   const tenant = 'jimmy';
+
+  const data=await getData(params.domain);
+  // eslint-disable-next-line no-console
+  console.log('bot config ',data)
 
   const Home = layouts.home[tenant];
 
   return (
     <div>
-      <p>Test Page {params.domain}</p>
+      <p>Test Page {params.domain} {JSON.stringify(data)}</p>
       <Home />
     </div>
   );
 };
+
+
 
 export default HomePage;
--- src/constant/layouts.ts
+++ src/constant/layouts.ts
@@ -14,4 +14,7 @@ export const layouts = {
     jimmy: AboutPageJimmy,
     bethany: AboutBethany,
   },
+  default:{
+    
+  }
 };
