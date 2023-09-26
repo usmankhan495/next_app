2bb4d8be4c047fbd5f4e01a567bb78700bcdace5
Usman Khalid <ukhalid@jobcase.com>
2023-09-26 15:53:06 +0500

    config api added

--- src/app/[domain]/page.tsx
+++ src/app/[domain]/page.tsx
@@ -3,7 +3,7 @@ import { layouts } from '@/constant/layouts';
 async function getData(domain:string) {
   // eslint-disable-next-line no-console
   console.log('getData',  domain)
-  const res = await fetch(`https://blazorstagingapi.azurewebsites.net/api/Config?URL=https://next-app-two-ruddy.vercel.app/`)
+  const res = await fetch(`https://blazorstagingapi.azurewebsites.net/api/Config?URL=${domain}`)
  
   if (!res.ok) {
   //  This will activate the closest `error.js` Error Boundary
@@ -18,7 +18,7 @@ export default async function HomePage({ params }: { params: { domain: string } 
   // BE
   const tenant = 'jimmy';
 
-  const data=await getData(params.domain);
+  const data=await getData('https://'+params.domain+'/');
   // eslint-disable-next-line no-console
   console.log('bot config ',data)
 
